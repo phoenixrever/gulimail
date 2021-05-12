2372185e43150da3165b000c2e3dd6dc2962b984
phoenixhell <phoenixrever@gmail.com>
2021-05-12 21:33:38 +0800

    2021-05-12 first blood

---
+++ gulimall-order/src/test/java/com/phoenixhell/mail/order/GulimailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixhell.mail.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
